0b3a09456b90850311bee4ff8cf80aa8391c3ea6
leaserr <83015115+leaserr@users.noreply.github.com>
2023-09-04 23:55:21 +0300

    s

--- beş_client.js
+++ beş_client.js
@@ -1,6 +1,7 @@
-const { Client, GatewayIntentBits, Partials,ActivityType,Events } = require('discord.js');
-const { joinVoiceChannel } = require('@discordjs/voice')
-let conf = require('./beş_config');
+﻿const { Client,  VoiceChannel, GuildMember, PermissionFlagsBits, GatewayIntentBits, Partials,ActivityType,Events } = require('discord.js');
+const conf = require('./beş_config.js');
+const {createAudioPlayer,createAudioResource,NoSubscriberBehavior,AudioPlayerStatus} = require('@discordjs/voice');
+const play = require('play-dl');
 
 class BEŞ extends Client {
     constructor(options) {
@@ -18,18 +19,15 @@ class BEŞ extends Client {
               }
         })
 
-        this.on(Events.ClientReady, () => {
-          let channel = this.guilds.cache.get(conf.guildID).channels.cache.get(conf.voiceChannelID)
-          joinVoiceChannel({
-          channelId: channel.id,
-          guildId: channel.guild.id,
-          adapterCreator: channel.guild.voiceAdapterCreator,
-          group: this.user.id,
-          selfDeaf:true,
-          selfMute:true
-          });
-        })
-         
+        this.player = createAudioPlayer({inlineVolume : true,behaviors: {noSubscriber: NoSubscriberBehavior.Play,},});
+        this.url = conf.youtubeURL;
+        this.stream;
+        this.message;
+        this.channelId;
+        this.playing;
+        this.voiceConnection;
+        this.staffJoined = false;
+        
          process.on("uncaughtException", (err) => { });
          process.on("unhandledRejection", (err) => { console.log(err) });
          process.on("warning", (warn) => { console.log(warn) });
@@ -39,6 +37,46 @@ class BEŞ extends Client {
          this.on(Events.Warn,(warn) => { })
         
     }
+    async start(channelId, a) {
+        let guild = this.guilds.cache.get(conf.guildID);
+        if(!guild) return;
+        let channel = guild.channels.cache.get(channelId);
+        if(!channel) return;
+        this.channelId = channelId;
+        let connection = this.voiceConnection 
+        let stream;
+        let resource;
+        if(conf.mp3) {
+        resource = this.stream = createAudioResource(conf.file); 
+        } else {
+        stream = await play.stream(this.url);
+        resource = this.stream = createAudioResource(stream.stream, {inputType: stream.type,}); 
+        }//slm acr
+        let player = this.player
+        player.on(AudioPlayerStatus.Playing, () => {});
+        player.on(AudioPlayerStatus.Paused, () => {});
+        player.on('idle', async () => {
+        if(this.staffJoined == true) return;
+        if(conf.mp3) {resource = this.stream = createAudioResource(conf.file); 
+        } else {stream = await play.stream(this.url);
+        resource = this.stream = createAudioResource(stream.stream, {inputType: stream.type,}); 
+        }
+        this.player.play(resource);
+        });
+        if(this.staffJoined == true) return;
+        player.play(resource)
+        connection.subscribe(player);
+    }
 }
-
 module.exports = { BEŞ };
+VoiceChannel.prototype.hasStaff = function(checkMember = false) {
+if(this.members.some(m => (checkMember !== false ? m.user.id !== checkMember.id : true) && !m.user.bot && m.roles.highest.position >= m.guild.roles.cache.get(conf.staffRole).position)) return true;
+return false;
+}
+VoiceChannel.prototype.getStaffs = function(checkMember = false) {
+return this.members.filter(m => (checkMember !== false ? m.user.id !== checkMember.id : true) && !m.user.bot && m.roles.highest.position >= m.guild.roles.cache.get(conf.staffRole).position).size
+}
+GuildMember.prototype.isStaff = function() {
+if(!this.user.bot && (this.permissions.has(PermissionFlagsBits.Administrator) || this.roles.highest.position >= this.guild.roles.cache.get(conf.staffRole).position)) return true;
+return false;
+}
